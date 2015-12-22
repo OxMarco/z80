fc4d892b4b94aa097e4168cdaff4040a3e2f0536
Giammarco R. Casanova <grcasanova@users.noreply.github.com>
2015-12-22 20:46:08 +0100

    Update output.asm

--- output.asm
+++ output.asm
@@ -1,9 +1,13 @@
+; runs an infinite loop and prints, after 1400*T seconds (T = clock period)
+; and it increments the value of A every external cycle iteration
+; then it prints out the value stored in A on the default output pin (Q1)
+
+ld A, 0h
 start:
-ld A, 0
-ld B, 100
-loop:
-dec B
-jp nz, loop
-add A, 10
+ld B, 64h
+    loop:
+    dec B
+    jp nz, loop
+add A, 01h
 out(01), A
 jp start
