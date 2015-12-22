db18fb0c939dafaa6f31af686149f2f4358972b8
Giammarco R. Casanova <grcasanova@users.noreply.github.com>
2015-12-22 09:42:54 +0100

    Create nested_loops.asm

---
+++ nested_loops.asm
@@ -0,0 +1,16 @@
+; this code execution time is 10 seconds, due to the three nested loops 
+; 100*[100*[70*14+14]+14]*T = 10 sec where T is the clock set at 1 MHz
+
+ld a, 46h
+loop1:
+   ld b, 64h
+   loop2:
+      ld c, 64h
+         loop3:
+         dec c
+         jnz c, loop3
+   dec b
+   jnz b, loop2
+dec a
+jnz a, loop1
+halt
