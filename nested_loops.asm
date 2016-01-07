5779974478a65f1ce4036ec27b19b1a535fe1a87
Giammarco R. Casanova <grcasanova@users.noreply.github.com>
2016-01-07 15:12:51 +0100

    switched loops duration

--- nested_loops.asm
+++ nested_loops.asm
@@ -1,11 +1,11 @@
 ; this code execution time is 10 seconds, due to the three nested loops 
 ; 100*[100*[70*14+14]+14]*T = 10 sec where T is the clock set at 1 MHz
 
-ld a, 46h
+ld a, 64h
 loop1:
    ld b, 64h
    loop2:
-      ld c, 64h
+      ld c, 46h
          loop3:
          dec c
          jnz c, loop3
