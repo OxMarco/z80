f79065f12dedf7dde081f529e51a768ea18a40fa
Giammarco R. Casanova <grcasanova@users.noreply.github.com>
2016-01-11 17:22:45 +0100

    Update and rename dac_calibration.asm to dac.asm

--- dac.asm
+++ dac.asm
@@ -3,7 +3,13 @@
 ; WIRING: Q0 to the buffer clock input pin, Q1 I/O bus to the display and Q2 to the keyboard
 ; use a DAC08 with a 74LS374 as middle flip-flop edge-triggered buffer
 
-db 02
-d3 00
-d3 01
-c3 00 00
+start:
+in A, (02)
+inc A
+out (00), A
+out (01), A
+jp start
+
+; this program generates a rising triangle wave
+
+
